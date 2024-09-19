42116b8c53ae4f2f78494dc4ed189be99ea61e08
Cooper Shine <166638981+shinecoop@users.noreply.github.com>
2024-09-19 19:20:34 +0000

    roman numerals

--- Romans.java
+++ Romans.java
@@ -12,12 +12,18 @@ public class Romans
 	{
 		m = new TreeMap<String, Integer>();
 		
-		//complete your map				
+		m.put("I", 1);
+		m.put("V", 5);
+		m.put("X",10);
+		m.put("L",50);
+		m.put("C",100);
+		m.put("D",500);
+		m.put("M",1000);			
 	}
 	
 	public int getNumber( String s )
 	{
-		//complete this method
+		return(m.get(s));
 	}
 
 }
